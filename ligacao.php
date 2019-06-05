192fc256061c3e587003466eee82270b2d0709ca
marianadoesart <51413309+marianadoesart@users.noreply.github.com>
2019-06-05 16:55:11 +0100

    Add files via upload

---
+++ ligacao.php
@@ -0,0 +1,12 @@
+<?php
+
+	
+
+$baseDados="";
+$utilizador="root";
+$password="";
+$servidor = "localhost";
+$ligacao = mysqli_connect($servidor,$utilizador,$password,$baseDados) or  die ("erro1");
+
+
+?>
